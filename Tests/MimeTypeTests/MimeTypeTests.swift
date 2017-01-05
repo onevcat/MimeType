b34f7f86ba31ddf48b12dd9bd6286726e039b01f
onevcat <onevcat@gmail.com>
2017-01-05 21:29:41 +0900

    Implement mimetype

--- Tests/MimeTypeTests/MimeTypeTests.swift
+++ Tests/MimeTypeTests/MimeTypeTests.swift
@@ -2,16 +2,33 @@ import XCTest
 @testable import MimeType
 
 class MimeTypeTests: XCTestCase {
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        XCTAssertEqual(MimeType().text, "Hello, World!")
+    func testMimeTypeFromString() {
+        let path = "/User/onevcat/hello.html"
+        XCTAssertEqual(path.mimeType, "text/html")
     }
 
+    func testMimeTypeFromURL() {
+        let path = "/User/onevcat/hello.html"
+        let url = URL(fileURLWithPath: path)
+        XCTAssertEqual(url.mimeType, "text/html")
+    }
+
+    func testMimeTypeFromNSString() {
+        let path: NSString = "/User/onevcat/hello.html"
+        XCTAssertEqual(path.mimeType, "text/html")
+    }
+
+    func testMimeTypeDefaultType() {
+        let path = "/User/onevcat/hello.xyz"
+        XCTAssertEqual(path.mimeType, "application/octet-stream")
+    }
 
     static var allTests : [(String, (MimeTypeTests) -> () throws -> Void)] {
         return [
-            ("testExample", testExample),
+            ("testMimeTypeFromString", testMimeTypeFromString),
+            ("testMimeTypeFromURL", testMimeTypeFromURL),
+            ("testMimeTypeFromNSString", testMimeTypeFromNSString),
+            ("testMimeTypeDefaultType", testMimeTypeDefaultType)
         ]
     }
 }
